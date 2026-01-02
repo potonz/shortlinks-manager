0248a8ae566e5c0aa75dcaa9145dcb2dafdc3bcb
ducng99 <tom@tomng.dev>
2026-01-03 03:23:54 +1300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ export default defineConfig([
     {
         files: ["**/*.{js,mjs,cjs,ts}"],
         languageOptions: {
-            globals: globals.node,
+            globals: globals.bunBuiltin,
         },
     },
     {
